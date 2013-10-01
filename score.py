0cd593ad4e33f9f0024850f7b72a1e2170ebb3f3
Erik Schoster <erik@heythere.org>
2013-10-01 18:29:11 -0500

    Trying some variation over time out.

--- score.py
+++ score.py
@@ -4,11 +4,17 @@ import rhodes
 
 freqs = tune.fromdegrees([1,3,5,6,9], 3, 'c')
 
-layers = []
-for i in range(3):
-    f = [ dsp.randchoose(freqs) for f in range(3) ]
-    layers += [ ''.join([ rhodes.rhodes(dsp.mstf(dsp.rand(80, 90)), f[ ii % len(f) ]) for ii in range(dsp.randint(10, 20)) ]) ]
+out = ''
 
-out = dsp.mix(layers)
+f = [ dsp.randchoose(freqs) for f in range(3) ]
+for r in range(20):
+    layers = []
+    for i in range(3):
+
+        if r % 5 == 0:
+            f = [ dsp.randchoose(freqs) for f in range(3) ]
+        layers += [ ''.join([ rhodes.rhodes(dsp.mstf(dsp.rand(80, 90)), f[ ii % len(f) ]) for ii in range(dsp.randint(10, 20)) ]) ]
+
+    out += dsp.mix(layers)
 
 dsp.write(out, 'riley')
