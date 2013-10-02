d73a14de5c0c61730fc364d566398ec59103a5e7
Erik Schoster <erik@heythere.org>
2013-10-02 00:27:55 -0500

    trying some chord constructions on root movement by thirds...

--- score.py
+++ score.py
@@ -2,18 +2,56 @@ from pippi import dsp
 from pippi import tune
 import rhodes
 
-freqs = tune.fromdegrees([0,1,2,3,4,5,6,9], 3, 'c', scale=range(12))
+#freqs = tune.fromdegrees([0,1,2,3,4,5,6,9], 3, 'c', scale=range(12))
+
+# interval map for chords...
+chords = {
+        1: [1, 4, 7],
+        2: [2, 5, 8],
+        3: [3, 6, 9],
+        4: [4, 7, 10],
+        5: [5, 8, 11], 
+        6: [6, 9, 12], 
+        7: [7, 10, 1], 
+        8: [8, 11, 2], 
+        9: [9, 12, 3],
+        10: [10, 1, 4], 
+        11: [11, 2, 5],
+        12: [12, 3, 6],
+        }
+
+def choose_interval(lastroot):
+    interval = dsp.randchoose([3, 4, -3, -4])
+    if interval + lastroot >= 1 and interval + lastroot <= 12:
+        nextroot = lastroot + interval
+    else:
+        nextroot = choose_interval(lastroot)
+
+    return nextroot
+
+roots = []
+lastroot = 1
+for root in range(12):
+    roots += [ lastroot ]
+    nextroot = choose_interval(lastroot)
+    lastroot = nextroot
+
+print roots
+
+freqs = [ tune.fromdegrees([ cr - 1 for cr in chords[root]], 3, 'c', scale=range(12)) for root in roots ]
 
 out = ''
 
-f = [ dsp.randchoose(freqs) for f in range(3) ]
+#f = [ dsp.randchoose(freqs) for f in range(3) ]
 
 for r in range(50):
     layers = []
 
     for i in range(3):
-        if r % 5 == 0:
-            f = [ dsp.randchoose(freqs) for f in range(3) ]
+        #if r % 5 == 0:
+            #f = [ dsp.randchoose(freqs) for f in range(3) ]
+
+        f = freqs[r % len(freqs)]
 
         notes = []
         for n in range(dsp.randint(10, 20)):
